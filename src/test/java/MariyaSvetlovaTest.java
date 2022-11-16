b6ffa69712bec8b3620b007109adcf7ff7b8598d
MariyaSvetlova <lmt3103@mail.ru>
2022-11-17 02:28:49 +0500

    added spaces

--- src/test/java/MariyaSvetlovaTest.java
+++ src/test/java/MariyaSvetlovaTest.java
@@ -5,7 +5,9 @@ import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class MariyaSvetlovaTest extends BaseTest {
+
     static final String BACE_URL = "https://www.99-bottles-of-beer.net/";
+
     @Test
     public void testVerifyHeaderH1OnStartPage() {
         final String HEADER_H1 = "99 Bottles of Beer";
