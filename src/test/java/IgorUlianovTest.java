966647e1451761b81d922abede844d271131d578
Igor Ulianov <igorulianov>
2022-11-16 12:21:04 -0500

    testBrowseLanguagesByCategoryName_HappyPath() added

--- src/test/java/IgorUlianovTest.java
+++ src/test/java/IgorUlianovTest.java
@@ -8,32 +8,29 @@ import java.util.List;
 
 public class IgorUlianovTest extends BaseTest {
     @Test
-    public void testSearchForLanguageByName_HappyPath() {
+    public void testBrowseLanguagesByCategoryName_HappyPath() {
         final String BASE_URL = "https://www.99-bottles-of-beer.net/";
-        final String LANGUAGE_NAME = "python";
+        final String CATEGORY_NAME = "x";
 
         getDriver().get(BASE_URL);
 
-        WebElement searchLanguageMenu = getDriver().findElement(
-                By.xpath("//ul[@id = 'menu']/li/a[@href = '/search.html']")
+        WebElement browseLanguagesMenu = getDriver().findElement(
+                By.xpath("//ul[@id = 'menu']//a[@href='/abc.html']")
         );
-        searchLanguageMenu.click();
+        browseLanguagesMenu.click();
 
-        WebElement searchForField = getDriver().findElement(By.name("search"));
-        searchForField.click();
-        searchForField.sendKeys(LANGUAGE_NAME);
-
-        WebElement goButton = getDriver().findElement(By.name("submitsearch"));
-        goButton.click();
+        WebElement searchForCategory = getDriver().findElement(
+                By.xpath("//ul[@id = 'submenu']//a[@href='x.html']")
+        );
+        searchForCategory.click();
 
         List<WebElement> languagesNamesList = getDriver().findElements(
-                By.xpath("//table[@id = 'category']/tbody/tr/td[1]/a"));
+                By.xpath("//table[@id = 'category']/tbody//td[1]/a"));
 
         Assert.assertTrue(languagesNamesList.size() > 0);
 
         for (int i = 0; i < languagesNamesList.size(); i++) {
-            Assert.assertTrue(languagesNamesList.get(i).getText().toLowerCase().contains(LANGUAGE_NAME));
-
+            Assert.assertTrue(languagesNamesList.get(i).getText().toLowerCase().startsWith(CATEGORY_NAME));
         }
     }
 }
