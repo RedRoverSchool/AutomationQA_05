2f47161ccdf54eda3a43b671ec62309e1f078c61
MariyaSvetlova <lmt3103@mail.ru>
2022-11-17 01:41:20 +0500

    ATMainHeaderH1_testVerifyHeaderOnStartPage_MariyaSvetlova

---
+++ src/test/java/MariyaSvetlovaTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+
+public class MariyaSvetlovaTest extends BaseTest {
+    //static final String BACE_URL = "https://www.99-bottles-of-beer.net/";
+
+    @Test
+    public void testVerifyHeaderH1OnStartPage() {
+        final String HEADER_H1 = "99 Bottles of Beer";
+
+        //getDriver().
+
+
+    }
+}
