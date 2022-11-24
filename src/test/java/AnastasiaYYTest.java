5062e3e410d51987b59596d7c33871b5beae2adc
Tatyana Belikova <t.belikova.oodji@gmail.com>
2022-11-24 08:41:55 +0300

    Resolved merge conflict by incorporating both suggestions.

--- src/test/java/AnastasiaYYTest.java
+++ src/test/java/AnastasiaYYTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,21 +12,65 @@ import java.util.List;
 public class AnastasiaYYTest extends BaseTest {
 
     final static String BASE_URL = "https://www.99-bottles-of-beer.net/";
+    final static By SEARCH_LANGUAGES_MENU = By.xpath("//ul[@id = 'menu']/li/a[@href = '/search.html']");
+    final static By SEARCH_FOR_FIELD = By.name("search");
+    final static By GO_BUTTON = By.name("submitsearch");
+    final static By LANGUAGES_NAMES_LIST = By.xpath("//table[@id = 'category']/tbody/tr/td[1]/a");
+    final static By H3_HEADER_NAMES = By.xpath("//div[@id='main']/h3");
     final static By START_MENU = By.xpath("//ul[@id = 'menu']//a[text() = 'Start']");
     final static By TOPLISTS_MENU = By.xpath("//ul[@id = 'menu']//a[text() = 'Top Lists']");
     final static By TEAM_SUBMENU = By.cssSelector("#submenu a[href = 'team.html']");
 
-    private void openBaseURL() {
-        getDriver().get(BASE_URL);
+    private void openBaseURL(WebDriver driver) {
+        driver.get(BASE_URL);
     }
 
-    private List<String> WebElementToString(List<WebElement> elementList) {
-        List<String> stringList = new ArrayList<>();
-        for (WebElement element : elementList) {
-            stringList.add(element.getText());
+    private WebElement getElement(By by, WebDriver driver) {
+
+        return driver.findElement(by);
+    }
+
+    private List<WebElement> getListOfElements(By by, WebDriver driver) {
+
+        return driver.findElements(by);
+    }
+
+    private void click(By by, WebDriver driver) {
+        getElement(by, driver).click();
+    }
+
+    private void input(String text, By by, WebDriver driver) {
+        getElement(by, driver).sendKeys(text);
+    }
+
+    private int getListSize(By by, WebDriver driver) {
+
+        return getListOfElements(by, driver).size();
+    }
+
+    private int getListSize(List<String> list) {
+
+        return list.size();
+    }
+
+    private List<String> getElementsTextLowerCase(By by, WebDriver driver) {
+        List<WebElement> elementsList = getListOfElements(by, driver);
+        List<String> textList = new ArrayList<>();
+        for (WebElement element: elementsList) {
+            textList.add(element.getText().toLowerCase());
         }
 
-        return stringList;
+        return textList;
+    }
+
+    private List<String> getElementsText(By by, WebDriver driver) {
+        List<WebElement> elementsList = getListOfElements(by, driver);
+        List<String> textList = new ArrayList<>();
+        for (WebElement element: elementsList) {
+            textList.add(element.getText());
+        }
+
+        return textList;
     }
 
     @Test
@@ -33,25 +78,18 @@ public class AnastasiaYYTest extends BaseTest {
 
         final String LANGUAGE_NAME = "python";
 
-        openBaseURL();
+        openBaseURL(getDriver());
+        click(SEARCH_LANGUAGES_MENU, getDriver());
+        click(SEARCH_FOR_FIELD, getDriver());
+        input(LANGUAGE_NAME, SEARCH_FOR_FIELD, getDriver());
+        click(GO_BUTTON, getDriver());
 
-        WebElement searchLanguagesMenu = getDriver().findElement(
-                By.xpath("//ul[@id = 'menu']/li/a[@href = '/search.html']"));
-        searchLanguagesMenu.click();
+        List<String> languageNames = getElementsTextLowerCase(LANGUAGES_NAMES_LIST, getDriver());
 
-        WebElement searchForField = getDriver().findElement(By.name("search"));
-        searchForField.click();
-        searchForField.sendKeys(LANGUAGE_NAME);
+        Assert.assertTrue(languageNames.size() > 0);
 
-        WebElement goButton = getDriver().findElement(By.name("submitsearch"));
-        goButton.click();
-
-        List<WebElement> languagesNamesList = getDriver().findElements(By.xpath("//table[@id = 'category']/tbody/tr/td[1]/a"));
-
-        Assert.assertTrue(languagesNamesList.size() > 0);
-
-        for (int i = 0; i < languagesNamesList.size(); i++) {
-            Assert.assertTrue(languagesNamesList.get(i).getText().toLowerCase().contains(LANGUAGE_NAME));
+        for (String languageName: languageNames) {
+            Assert.assertTrue(languageName.contains(LANGUAGE_NAME));
         }
     }
 
@@ -60,16 +98,11 @@ public class AnastasiaYYTest extends BaseTest {
         List<String> expectedH3headerNames = new ArrayList<>(
                 Arrays.asList("Oliver Schade", "Gregor Scheithauer", "Stefan Scheler"));
 
-        openBaseURL();
+        openBaseURL(getDriver());
+        click(START_MENU, getDriver());
+        click(TEAM_SUBMENU, getDriver());
 
-        WebElement startMenu = getDriver().findElement(START_MENU);
-        startMenu.click();
-
-        WebElement teamSubMenu = getDriver().findElement(TEAM_SUBMENU);
-        teamSubMenu.click();
-
-        List<WebElement> h3HeaderNames = getDriver().findElements(By.xpath("//div[@id='main']/h3"));
-        List<String> actualH3headerNames = WebElementToString(h3HeaderNames);
+        List<String> actualH3headerNames = getElementsText(H3_HEADER_NAMES, getDriver());
 
         Assert.assertEquals(actualH3headerNames, expectedH3headerNames);
     }
@@ -79,7 +112,7 @@ public class AnastasiaYYTest extends BaseTest {
 
         final String expectedResult = "Top Rated";
 
-        openBaseURL();
+        openBaseURL(getDriver());
 
         WebElement topListsMenu = getDriver().findElement(TOPLISTS_MENU);
         topListsMenu.click();
--- src/test/java/LarisaMokTest.java
+++ src/test/java/LarisaMokTest.java
@@ -4,16 +4,16 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class SafOlgaTest extends BaseTest {
-    final String BASE_URL = "https://www.99-bottles-of-beer.net/";
-    final static By SEARCH_LANGUAGES_MENU =  By.xpath("//ul[@id = 'menu']/li/a[@href = '/search.html']");
+public class LarisaMokTest extends BaseTest {
+    final String BASE_URL = "https://99-bottles-of-beer.net/";
+    final static By SEARCH_LANGUAGES_MENU = By.xpath("//ul[@id='menu']/li/a[@href='/search.html']");
     final static By SEARCH_FOR_FIELD = By.name("search");
     final static By GO_BUTTON = By.name("submitsearch");
     final static By LANGUAGES_NAMES_LIST = By.xpath("//table[@id='category']/tbody/tr/td[1]/a");
-    private final By FOOTER_BAR = By.id("footer");
 
     private void openBaseURL(WebDriver driver) {
         driver.get(BASE_URL);
@@ -23,9 +23,11 @@ public class SafOlgaTest extends BaseTest {
 
         return driver.findElement(by);
     }
+
     private void click(By by, WebDriver driver) {
         getElement(by, driver).click();
     }
+
     private void input(String text, By by, WebDriver driver) {
         getElement(by, driver).sendKeys(text);
     }
@@ -35,18 +37,23 @@ public class SafOlgaTest extends BaseTest {
         return driver.findElements(by);
     }
 
+    private int getListSize(By by, WebDriver driver) {
+
+        return getListOfElements(by, driver).size();
+    }
+
     private List<String> getElementsText(By by, WebDriver driver) {
         List<WebElement> elementsList = getListOfElements(by, driver);
         List<String> textList = new ArrayList<>();
-
         for (WebElement element : elementsList) {
             textList.add(element.getText().toLowerCase());
         }
 
         return textList;
     }
+
     @Test
-    public void testSearchForLanguagesByName_HappyPath() {
+    public void testSearchForLanguageByName_HappyPath() {
 
         final String LANGUAGE_NAME = "python";
 
@@ -63,17 +70,5 @@ public class SafOlgaTest extends BaseTest {
         for (String languageName : languageNames) {
             Assert.assertTrue(languageName.contains(LANGUAGE_NAME));
         }
-    }
-
-    @Test
-    public void testFooterMenuBarMainPage() {
-
-        String expectedResult = "Start | Browse Languages | Search Languages | Top Lists | Guestbook | Submit new Language";
-
-        openBaseURL(getDriver());
-
-        WebElement actualResult = getDriver().findElement(FOOTER_BAR);
-
-        Assert.assertEquals(actualResult.getText(), expectedResult);
     }
 }
