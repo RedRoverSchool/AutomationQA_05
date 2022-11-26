062210b931f2828191fe0398652b895d9e6f7fbc
Ana-Lara <analara1303@gmail.com>
2022-11-25 23:29:50 -0700

    testSearchLanguageField_HappyPath() refactored, locators and methods added

--- src/test/java/AnaLaraTest.java
+++ src/test/java/AnaLaraTest.java
@@ -1,44 +1,80 @@
-import org.apache.commons.codec.language.bm.Lang;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AnaLaraTest extends BaseTest {
 
-    @Test
-    public void testSearchLanguageField_HappyPath() {
+    final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+    final static By SEARCH_LANGUAGES_MENU = By.xpath("//ul[@id='menu']/li/a[@href = '/search.html']");
+    final static By SEARCH_FOR_FIELD = By.name("search");
+    final static By GO_BUTTON = By.name("submitsearch");
+    final static By LANGUAGES_NAME_LIST = By.xpath("//table[@id = 'category']/tbody/tr/td[1]/a");
 
-        final String BASE_URL = "https://www.99-bottles-of-beer.net/";
-        final String LANGUAGE_NAME = "python";
-
-        getDriver().get(BASE_URL);
-
-        WebElement searchLanguagesMenu = getDriver().findElement(By.xpath("//ul[@id='menu']/li/a[@href = '/search.html']")
-        );
-        searchLanguagesMenu.click();
-
-        WebElement searchForField = getDriver().findElement(By.name("search"));
-        searchForField.click();
-        searchForField.sendKeys(LANGUAGE_NAME);
-
-        WebElement goButton = getDriver().findElement(By.name("submitsearch"));
-        goButton.click();
-
-        List<WebElement> languagesNamesList = getDriver().findElements(
-                By.xpath("//table[@id = 'category']/tbody/tr/td[1]/a")
-        );
-        Assert.assertTrue(languagesNamesList.size() > 0);
-
-        for (int i = 0; i < languagesNamesList.size(); i++) {
-            Assert.assertTrue(languagesNamesList.get(i).getText().toLowerCase().contains(LANGUAGE_NAME));
-        }
+    private void openBaseURL(WebDriver driver) {
+        driver.get(BASE_URL);
     }
+
+    private WebElement getElement(By by, WebDriver driver) {
+        return driver.findElement(by);
+    }
+
+    private List<WebElement> getListOfElements(By by, WebDriver driver) {
+        return driver.findElements(by);
+    }
+
+    private void click(By by, WebDriver driver) {
+        getElement(by, driver).click();
+    }
+
+    private void input(String text, By by, WebDriver driver) {
+        getElement(by, driver).sendKeys(text);
+    }
+
+    private int getListSize(By by, WebDriver driver) {
+        return getListOfElements(by, driver).size();
+    }
+
+    private int getListSize(List<String> list) {
+        return list.size();
+    }
+
+    private List<String> getElementsText(By by, WebDriver driver) {
+        List<WebElement> elementsList = getListOfElements(by, driver);
+        List<String> textList = new ArrayList<>();
+
+        for (WebElement element : elementsList) {
+            textList.add(element.getText().toLowerCase());
+        }
+        return textList;
+
         @Test
-        public void testNavigationToLyricsSubmenu_HappyPath() {
+        public void testSearchLanguageField_HappyPath () {
+            final String LANGUAGE_NAME = "python";
+
+            openBaseURL(getDriver());
+            click(SEARCH_LANGUAGES_MENU, getDriver());
+            click(SEARCH_FOR_FIELD, getDriver());
+            input(LANGUAGE_NAME, SEARCH_FOR_FIELD, getDriver());
+            click(GO_BUTTON, getDriver());
+            List<WebElement> languagesNameList = getListOfElements(LANGUAGES_NAME_LIST, getDriver());
+
+            List<String> languageNames = getElementsText(LANGUAGES_NAME_LIST, getDriver());
+
+            Assert.assertTrue(languageNames.size() > 0);
+
+            for (String languageName : languageNames) {
+                Assert.assertTrue(languageName.contains(LANGUAGE_NAME));
+            }
+        }
+
+        @Test
+        public void testNavigationToLyricsSubmenu_HappyPath () {
 
             final String BASE_URL2 = "https://www.99-bottles-of-beer.net/";
             final String EXPECTED_URL = "https://www.99-bottles-of-beer.net/lyrics.html";
@@ -55,4 +91,5 @@ public class AnaLaraTest extends BaseTest {
             Assert.assertEquals(ACTUAL_URL, EXPECTED_URL);
         }
     }
+}
 
