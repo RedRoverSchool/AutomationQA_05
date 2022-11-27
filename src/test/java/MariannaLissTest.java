5445d82c0d099976d055c58976f17aafe607e54b
MariannaLiss <mlisitsyna88@gmail.com>
2022-11-27 03:02:17 +0300

    testClickLetterInSubmenu_HappyPath() added

--- src/test/java/MariannaLissTest.java
+++ src/test/java/MariannaLissTest.java
@@ -1,10 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,8 @@ public class MariannaLissTest extends BaseTest {
     final static By SEARCH_FOR_FIELD = By.name("search");
     final static By GO_BUTTON = By.name("submitsearch");
     final static By LANGUAGES_NAMES_LIST = By.xpath("//table[@id = 'category']/tbody/tr/td[1]/a");
+    final static By BROWSE_LANGUAGES_MENU = By.xpath("//div[@id = 'navigation']//a[@href = '/abc.html']");
+    final static By DEFAULT_LETTER = By.xpath("//div/ul[@id='submenu']/li/a[@href='m.html']");
 
     private void openBaseURL(WebDriver driver) {
         driver.get(BASE_URL);
@@ -27,6 +30,16 @@ public class MariannaLissTest extends BaseTest {
     private List<WebElement> getListOfElements(By by, WebDriver driver) {
 
         return driver.findElements(by);
+    }
+
+    private String getText(By by, WebDriver driver) {
+
+        return driver.findElement(by).getText();
+    }
+
+    public void waitTextToBeChanged(By by, String text, WebDriver driver, WebDriverWait wait) {
+        wait.until(ExpectedConditions
+                .not(ExpectedConditions.textToBePresentInElement(driver.findElement(by), text)));
     }
 
     private void click(By by, WebDriver driver) {
@@ -74,6 +87,23 @@ public class MariannaLissTest extends BaseTest {
 
         for (String languageName : languageNames) {
             Assert.assertTrue(languageName.contains(LANGUAGE_NAME));
+        }
+    }
+
+    @Test
+    public void testClickLetterInSubmenu_HappyPath() {
+        final String LETTER_NAME = "m";
+
+        openBaseURL(getDriver());
+        click(BROWSE_LANGUAGES_MENU, getDriver());
+        click(DEFAULT_LETTER, getDriver());
+
+        List<WebElement> languagesNamesList = getDriver().findElements(LANGUAGES_NAMES_LIST);
+
+        Assert.assertTrue(languagesNamesList.size() > 0);
+
+        for (int i = 0; i < languagesNamesList.size(); i++) {
+            Assert.assertTrue(languagesNamesList.get(i).getText().toLowerCase().startsWith(LETTER_NAME));
         }
     }
 }
