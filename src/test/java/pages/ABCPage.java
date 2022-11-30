ae522ff1fc756f18618fead97341c6989cf20d6d
Sergey Kuznetsov <votkinsk@gmail.com>
2022-11-29 18:24:54 -0800

    Restore ABCPage.java

--- src/test/java/pages/ABCPage.java
+++ src/test/java/pages/ABCPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 
 public class ABCPage extends BrowseLanguagesSubmenuPage {
+
     public ABCPage(WebDriver driver) {
         super(driver);
     }
