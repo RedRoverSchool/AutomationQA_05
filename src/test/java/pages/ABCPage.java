ad123d632ebda26d8523a26aedd407253b37de3c
Irina Dudchenko <irinadudchenko67@gmail.com>
2022-12-01 09:35:13 +0100

    ABC test refactored using POM

--- src/test/java/pages/ABCPage.java
+++ src/test/java/pages/ABCPage.java
@@ -1,10 +1,20 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class ABCPage extends BrowseLanguagesSubmenuPage {
 
+    @FindBy(xpath = "//div[@id='main']/h2")
+    private WebElement textCategoryA;
+
     public ABCPage(WebDriver driver) {
         super(driver);
+    }
+
+    public String getTextH2() {
+
+        return textCategoryA.getText();
     }
 }
--- src/test/java/tests/ABCTest.java
+++ src/test/java/tests/ABCTest.java
@@ -1,6 +1,19 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.ABCPage;
 
 public class ABCTest extends BaseTest {
+    @Test
+    public void testTextABC() {
+        final String expectedResult = "Category A";
+
+        openBaseURL().clickBrowseLanguagesMenu();
+        ABCPage abcPage = new ABCPage(getDriver());
+        String actualResult = abcPage.getTextH2();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 }
