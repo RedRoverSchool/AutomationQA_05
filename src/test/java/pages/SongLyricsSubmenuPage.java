78970eaf9184a101e3ab8a623c4b2b72d523efd0
Anzhelika Baa <115436933+AnzhelikaBaa@users.noreply.github.com>
2022-11-30 20:30:29 -0500

    Update SongLyricsSubmenuPage.java

--- src/test/java/pages/SongLyricsSubmenuPage.java
+++ src/test/java/pages/SongLyricsSubmenuPage.java
@@ -5,8 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class SongLyricsSubmenuPage extends MainPage {
-    private static final String LYRICS_URL = "https://www.99-bottles-of-beer.net/lyrics.html";
-
+    
     @FindBy(xpath = "//ul[@id='submenu']/li/a[@href='lyrics.html']")
     private WebElement SongLyricsSubmenuPageTitle;
     public SongLyricsSubmenuPage(WebDriver driver) { super(driver); }
