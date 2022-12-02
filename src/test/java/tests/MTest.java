ccd30aec11aeb03407d1b8ba7b80c600745478ce
Aleksandr Akimov <gtwood@yandex.ru>
2022-12-02 08:00:53 +0300

    added a universal method of navigation in the submenu by symbol and test it on SymbolPage

--- src/test/java/tests/MTest.java
+++ src/test/java/tests/MTest.java
@@ -1,6 +1,22 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class MTest extends BaseTest {
+
+    @Test
+    public void testVerifyNavigationSymbolM() {
+        final String symbol = "M";
+        final String expectedResultTitle = "99 Bottles of Beer | Browse category M";
+        final String expectedResultCurrentUrl = "https://www.99-bottles-of-beer.net/m.html";
+
+        openBaseURL()
+                .clickBrowseLanguagesMenu()
+                .clickOnSymdolOnSubmenu(symbol);
+
+        Assert.assertEquals(getExternalPageTitle(), expectedResultTitle);
+        Assert.assertEquals(getExternalPageURL(), expectedResultCurrentUrl);
+    }
 }
--- src/test/java/tests/PTest.java
+++ src/test/java/tests/PTest.java
@@ -1,6 +1,22 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class PTest extends BaseTest {
+
+    @Test
+    public void testVerifyNavigationSymbolP() {
+        final String symbol = "P";
+        final String expectedResultTitle = "99 Bottles of Beer | Browse category P";
+        final String expectedResultCurrentUrl = "https://www.99-bottles-of-beer.net/p.html";
+
+        openBaseURL()
+                .clickBrowseLanguagesMenu()
+                .clickOnSymdolOnSubmenu(symbol);
+
+        Assert.assertEquals(getExternalPageTitle(), expectedResultTitle);
+        Assert.assertEquals(getExternalPageURL(), expectedResultCurrentUrl);
+    }
 }
