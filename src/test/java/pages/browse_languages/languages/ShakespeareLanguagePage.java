7b66947c11f54ce8775c0e21a7b048dc89e0489c
Kate Li <ekaterina.lizina87@gmail.com>
2022-12-06 21:13:55 -0500

    corrected ShakespeareLanguagePage extends LanguagePage

--- src/test/java/pages/browse_languages/languages/ShakespeareLanguagePage.java
+++ src/test/java/pages/browse_languages/languages/ShakespeareLanguagePage.java
@@ -3,7 +3,7 @@ package pages.browse_languages.languages;
 import org.openqa.selenium.WebDriver;
 import pages.top_lists.TopListsSubmenuPage;
 
-public class ShakespeareLanguagePage extends TopListsSubmenuPage {
+public class ShakespeareLanguagePage extends LanguagePage {
 
     public ShakespeareLanguagePage(WebDriver driver) {
         super(driver);
