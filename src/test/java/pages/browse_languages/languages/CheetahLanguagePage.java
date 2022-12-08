c12c7c6b5b46522ad6c2cc02b8a843ece3ab0092
Nataliia Oliver <psvnatali@gmail.com>
2022-12-08 10:04:45 -0600

    changed the inheritance of pages cheetahLCheetahLanguagePage and DelphiLanguagePage

--- src/test/java/pages/browse_languages/languages/CheetahLanguagePage.java
+++ src/test/java/pages/browse_languages/languages/CheetahLanguagePage.java
@@ -3,7 +3,7 @@ package pages.browse_languages.languages;
 import org.openqa.selenium.WebDriver;
 import pages.browse_languages.BrowseLanguagesSubmenuPage;
 
-public class CheetahLanguagePage extends BrowseLanguagesSubmenuPage {
+public class CheetahLanguagePage extends LanguagePage {
 
     public CheetahLanguagePage(WebDriver driver) {
         super(driver);
--- src/test/java/pages/browse_languages/languages/DelphiLanguagePage.java
+++ src/test/java/pages/browse_languages/languages/DelphiLanguagePage.java
@@ -3,7 +3,7 @@ package pages.browse_languages.languages;
 import org.openqa.selenium.WebDriver;
 import pages.browse_languages.BrowseLanguagesSubmenuPage;
 
-public class DelphiLanguagePage extends BrowseLanguagesSubmenuPage {
+public class DelphiLanguagePage extends LanguagePage {
 
     public DelphiLanguagePage(WebDriver driver) {
         super(driver);
