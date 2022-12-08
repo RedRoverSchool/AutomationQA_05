dfb5e586b853e2b2994c15c2ce8c5ac1a0e61fbf
Nataliia Oliver <psvnatali@gmail.com>
2022-12-08 09:38:03 -0600

    Created testDelphiLink_NavigatesTo_DelphiLanguagePage(), added locator to DPage, created metods on DPage for the test

---
+++ src/test/java/pages/browse_languages/languages/DelphiLanguagePage.java
@@ -0,0 +1,11 @@
+package pages.browse_languages.languages;
+
+import org.openqa.selenium.WebDriver;
+import pages.browse_languages.BrowseLanguagesSubmenuPage;
+
+public class DelphiLanguagePage extends BrowseLanguagesSubmenuPage {
+
+    public DelphiLanguagePage(WebDriver driver) {
+        super(driver);
+    }
+}
