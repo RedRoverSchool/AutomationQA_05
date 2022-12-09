818f19afa0e8c5318c5978d4e740d3b3911d1658
Anastasia Yakimova <anastasiayy@yahoo.com>
2022-12-08 23:01:54 -0500

    add testVotingNiceCodingIsSelected

--- src/test/java/pages/browse_languages/languages/YacasLanguagePage.java
+++ src/test/java/pages/browse_languages/languages/YacasLanguagePage.java
@@ -1,10 +1,20 @@
 package pages.browse_languages.languages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class YacasLanguagePage extends LanguagePage{
 
+    @FindBy(xpath = "//div[@id = 'voting']//option[5]")
+    private WebElement niceCodingRate;
+
     public YacasLanguagePage(WebDriver driver) {
         super(driver);
+    }
+
+    public String getVotingFieldText() {
+
+        return clickNiceCoding().getText(niceCodingRate);
     }
 }
