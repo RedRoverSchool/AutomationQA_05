935750c84c0233b7159dd2574835d34721fd3dd2
Irina Z Java <tester@test.com>
2022-12-10 17:29:41 -0500

    Selenium-java version chenged to 4.5.3

--- src/test/java/network/CaptureNetworkTraffic.java
+++ src/test/java/network/CaptureNetworkTraffic.java
@@ -3,8 +3,7 @@ package network;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v103.network.Network;
-
+import org.openqa.selenium.devtools.v106.network.Network;
 
 import java.util.ArrayList;
 import java.util.List;
