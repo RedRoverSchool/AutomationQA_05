cc203df5df449a7c9d83211b01f551e18619a497
Kate Li <ekaterina.lizina87@gmail.com>
2022-12-10 21:11:09 -0500

    test TopListsSubMenuIsVisible added

--- src/test/java/tests/TopHitsTest.java
+++ src/test/java/tests/TopHitsTest.java
@@ -1,15 +1,20 @@
 package tests;
 
 import base.BaseTest;
+import org.openqa.selenium.devtools.v101.domsnapshot.model.StringIndex;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.browse_languages.languages.ShakespeareLanguagePage;
 import pages.top_lists.TopHitsPage;
+import pages.top_lists.TopListsPage;
+import pages.top_lists.TopListsSubmenuPage;
+
+import java.util.List;
 
 public class TopHitsTest extends BaseTest {
 
     @Test
-    public void testShakespeareLink_NavigatesTo_ShakespeareLanguagePage(){
+    public void testShakespeareLink_NavigatesTo_ShakespeareLanguagePage() {
 
         final String expectedURL = "https://www.99-bottles-of-beer.net/language-shakespeare-664.html";
         final String expectedTitle = "99 Bottles of Beer | Language Shakespeare";
@@ -30,5 +35,24 @@ public class TopHitsTest extends BaseTest {
 
         Assert.assertEquals(actualUrl, expectedURL);
         Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test
+    public void testTopListsSubMenuIsVisible() {
+        int expectedCountSubMenuLinks = 7;
+        List<String> topListsSubMenuLinkTexts = List.of("Top Rated", "Top Rated Real"
+                , "Top Rated Esoteric", "Top Rated Assembly", "Top Hits"
+                , "New Languages this month", "New Comments");
+
+        int actualCountSubMenuLinks = openBaseURL()
+                .clickTopListsMenu()
+                .getSubmenusTexts().size();
+
+        List <String>  actualTopListSubMenuLinkTexts = openBaseURL()
+                .clickTopListsMenu()
+                .getSubmenusTexts();
+
+        Assert.assertEquals(actualCountSubMenuLinks, expectedCountSubMenuLinks);
+        Assert.assertEquals(actualTopListSubMenuLinkTexts, topListsSubMenuLinkTexts);
     }
 }
