c19efd620c2705380488b63848707fcdf32307e2
Aleksandr Akimov <gtwood@yandex.ru>
2022-12-15 00:50:32 +0300

    fix encode

--- src/test/java/tests/api/API_KotlinLanguageTest.java
+++ src/test/java/tests/api/API_KotlinLanguageTest.java
@@ -6,6 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.browse_languages.languages.KotlinLanguagePage;
 import utils.TestUtils;
+import java.net.URLEncoder;
 
 import java.util.List;
 
@@ -16,14 +17,15 @@ public class API_KotlinLanguageTest extends BaseTest {
     final static String NAME_INPUT_1 = "exampleid";
     final static String VALUE_INPUT1 = "2901";
     final static String NAME_INPUT_2 = "http_referer";
-    final static String VALUE_INPUT2 = "language-kotlin-2901.html";
+    final static String VALUE_INPUT2 = "/language-kotlin-2901.html";
     final static String NAME_SELECT = "rate";
     final static String VALUE_SELECT = "0";
     final static String NAME_INPUT_4 = "submitcomment";
     final static String VALUE_INPUT4 = "Vote";
 
-    final static String PAYLOAD = NAME_INPUT_1 + "=" + VALUE_INPUT1 + "&" + NAME_INPUT_2 + "=" + "%2F" + VALUE_INPUT2
-            + "&" + NAME_SELECT + "=" + VALUE_SELECT + "&" +  NAME_INPUT_4 + "=" +  VALUE_INPUT4;
+    final static String PAYLOAD = NAME_INPUT_1 + "=" + VALUE_INPUT1 + "&" + NAME_INPUT_2 + "="
+            + URLEncoder.encode(VALUE_INPUT2) + "&" + NAME_SELECT + "=" + VALUE_SELECT + "&" +  NAME_INPUT_4
+            + "=" +  VALUE_INPUT4;
     final static String PAGE_CONTEXT_BEFORE_REQUEST = "Voting\n" + "Error: Please select something.\n" + "Back";
 
     @Test
