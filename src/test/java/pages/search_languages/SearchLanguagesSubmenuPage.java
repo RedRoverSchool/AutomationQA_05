7ce0ccc7242498eae8c990775d1052e7c08344eb
Aleksandr Akimov <gtwood@yandex.ru>
2023-01-05 14:44:14 +0300

    added test with dataprovider for searchLanguagesSubmenu

--- src/test/java/pages/search_languages/SearchLanguagesSubmenuPage.java
+++ src/test/java/pages/search_languages/SearchLanguagesSubmenuPage.java
@@ -1,14 +1,23 @@
 package pages.search_languages;
 
+import TestData.TestData;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import pages.base_abstract.TablePage;
+import pages.start.StartPage;
+
+import java.util.List;
 
 public abstract class SearchLanguagesSubmenuPage extends TablePage<SearchLanguagesPage> {
 
     @FindBy(xpath = "//ul[@id='submenu']/li/a[@href='./search.html']")
     private WebElement searchSubmenu;
+
+    @FindBy(xpath = "//ul[@id='submenu']/li/a[@href]")
+    private List<WebElement> submanus;
 
     public SearchLanguagesSubmenuPage(WebDriver driver) {
         super(driver);
@@ -24,4 +33,15 @@ public abstract class SearchLanguagesSubmenuPage extends TablePage<SearchLanguag
 
         return new SearchLanguagesPage(getDriver());
     }
+
+    public List<WebElement> getSearchLanguagesSubmenu() {
+
+        return submanus;
+    }
+
+    public String getTextSearchLanguagesSubmenu(int index) {
+
+        return submanus.get(index).getText();
+    }
+
 }
